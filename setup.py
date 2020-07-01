f9535f4ef72e9e96aa0f4b097e2d6d1a0acc8319
Sam Sloniker <scoopgracie@gmail.com>
2020-07-01 15:50:36 -0700

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from distutils.core import setup
 setup(
-  name = 'ldtc',         # How you named your package folder (MyLib)
-  packages = ['ldtc'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Pure Python, zero-dependency language detector',   # Give a short description about your library
-  author = 'ScoopGracie',                   # Type in your name
-  author_email = 'scoopgracie@scoopgracie.com',      # Type in your E-Mail
-  url = 'https://github.com/scoopgracie/ldtc',   # Provide either the link to your github or to your website
-  keywords = ['nlp', 'text', 'classification'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-      ],
+  name = 'ldtc',
+  packages = ['ldtc'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Pure Python, zero-dependency language detector',
+  author = 'ScoopGracie',
+  author_email = 'scoopgracie@scoopgracie.com',
+  url = 'https://github.com/scoopgracie/ldtc',
+  keywords = ['nlp', 'text', 'classification'],
+  install_requires=[], 
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
